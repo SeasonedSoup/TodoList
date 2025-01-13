922498b8e703e7c1ee76d1b4ea72f7880e3a4666
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-01-13 16:28:13 +0800

    Create Class Project
    takes in a name and contains a function which adds a to do item to the
    project.

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,16 @@
 import { ToDoItem } from "./ToDoClass"
 
-const myProjects = []
+class Projects {
+    constructor(name) {
+        this.name = name
+        this.toDoItems = []
+    }
 
-const addtoDoToProject = (title, description, dueDate, priority) => {
-    const project = new ToDoItem(title, description, dueDate, priority)
-    myProjects.push(project)
+    addtoDoToProject = (title, description, dueDate, priority) => {
+    const toDoItem = new ToDoItem(title, description, dueDate, priority)
+    this.toDoItems.push(toDoItem)
+    }
 }
+
+//initial idea a class that stores the projects and manage which project should a to do list go, creating new projects etc.
+myProjects = []
