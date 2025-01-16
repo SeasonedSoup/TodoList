323dcc3523aa2f45c526ed119ffcc451595f7eb7
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-01-16 19:39:48 +0800

    Refactor Priorities Parameter
    you can now prevent possible different parameters and only includes
    low, medium and high as possible priorities else itll default to low

--- src/ToDoClass.js
+++ src/ToDoClass.js
@@ -1,14 +1,20 @@
 export class ToDoItem{
-    constructor(title, description, dueDate, priority){
+    constructor(title, description, dueDate, priority = 'low') {
+        const validPriorities = ['low', 'medium', 'high']
+
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
-        this.priority = priority;
+        this.priority = validPriorities.includes(priority.toLowerCase()) ? priority.toLowerCase() : 'low'; //checks if the prio para is included as a valid priority
         this.complete = false;
     }
 
     toggleComplete = () => {
         this.complete = !this.complete
+    }
+    //hmm how do i toggle priority
+    togglePriority = () => {
+
     }
 
 
