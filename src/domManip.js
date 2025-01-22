48e03b8f2368f97e2baf47992b2761740c5da19f
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-01-22 20:04:20 +0800

    Made Small Improvements on The committed
    its in dommanip.js just added few codes

--- src/domManip.js
+++ src/domManip.js
@@ -1,10 +1,15 @@
  export const displayForm = () => {
-    document.getElementById('add-todo-form').style.display = ''
+    document.getElementById('add-todo-form').style.display = '';
  }
 
  displayForm();
 
- // export const acceptUserInputChecklists
+export const acceptUserInputChecklists = () => {
+   const addItem = document.getElementById('insert-checklist').value;
+
+   //if the addItemValue contains something
+   /*Select the ul in html, createa new element, a span with the x icon  and reset the value after appending to the dom */
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { displayForm } from "./domManip";
 
+let clickEventsModule = (function() {
+
+    const addNewToDo = document.querySelector('.add-todo');
+    // addNewToDo.addEventListener('click', )
+
+    const addToCheckList = document.querySelector('.insert-checklist-button')
+    // addToChecklist.addEventListener('click', )
+})
 displayForm();
