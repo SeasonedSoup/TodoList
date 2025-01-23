8db793b99195a990626cbbd6fc8f67866e35742b
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-01-23 18:25:38 +0800

    Create Function add checklists to a certain to Do
    not much seeable progress but still good nonetheless

--- src/domManip.js
+++ src/domManip.js
@@ -7,6 +7,25 @@
 export const acceptUserInputChecklists = () => {
    const addItem = document.getElementById('insert-checklist').value;
 
+   if (addItem != '') {
+      const ul = document.querySelector('.lists-of-checklists')
+      const li = document.createElement('li')
+      li.textContent = addItem;
+
+      const span = document.createElement('span')
+      span.classList.add('remove-checklist-item');
+      const removeIcon = document.createTextNode('/u00D7');
+
+      span.appendChild(removeIcon)
+      li.appendChild(span)
+      ul.appendChild(li)
+
+      //reset
+      document.getElementById('insert-checklist').value = '';
+
+
+   }
+
    //if the addItemValue contains something
    /*Select the ul in html, createa new element, a span with the x icon  and reset the value after appending to the dom */
 }
