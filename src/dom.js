163d221fdb06e829a9bf29ea1f60c83f4985ea52
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-01-31 15:57:04 +0800

    Create Minimal Progress Code in dom.js
    will do a for each able to get the arr from instance of projects

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,23 @@
 import { ProjectFunc } from "./project";
 import { ToDoFunc } from "./todo";
 
-const ToDoDisplayFunc = (positionProject) => {  
-    const toDoContainer = document.querySelector('.container');
+const instanceOfProjects = ProjectFunc();
+
+//testing data will be named Default Project
+instanceOfProjects.createProject('Clean Dog') 
+
+
+const ProjectDisplay = () => {  
+    const projectContainer = document.querySelector('.container');
     
+    const projects = instanceOfProjects.getProjectArr();
+
+    projects.forEach()
+    
+    
+    //const h1 = document.createElement('h1');
+
+    
+    
+
 }
