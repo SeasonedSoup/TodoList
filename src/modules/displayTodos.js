2b72b9e8257b332e605d8a6cf0e7de6a28abda54
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-02-13 09:12:12 +0800

    Tested the ToDoDisplayFunc with Refactors
    i am now able to select the project position
    and append todos on the specific project

--- src/modules/displayTodos.js
+++ src/modules/displayTodos.js
@@ -11,21 +11,32 @@ export const bugCheck = () => {
 export const toDoDisplayFunc = (projectPosition) => {
     const instanceOfTodos = ToDoFunc();
     console.log("Todos instance:", instanceOfTodos);
-    
-
 
     const instanceOfProjectsFromTodo = ProjectFunc();
     console.log(instanceOfProjectsFromTodo.getProjectArr());
 
     const toDoContainer = document.querySelector('.toDoContainer');
     toDoContainer.textContent = '';
-
-    instanceOfTodos.insertToDoToProject(projectPosition, "Buy Groceries", "Milk, Eggs, Bread", "Tomorrow", "High");
-    instanceOfTodos.insertToDoToProject(projectPosition, "Study JavaScript", "Review closures and promises", "Next Week", "Medium");
-    instanceOfTodos.insertToDoToProject(projectPosition, "Workout", "Leg day at the gym", "Today", "Low");
+    
 
     if (projectPosition === null || projectPosition === undefined) {
         toDoContainer.innerHTML = "<p>No project selected.</p>";
         return;
     }
+
+    const todos = instanceOfTodos.selectToDo(projectPosition);
+
+    if(!todos || todos.length === 0) {
+        instanceOfTodos.insertToDoToProject(projectPosition, "Buy Groceries", "Milk, Eggs, Bread", "Tomorrow", "High");
+        instanceOfTodos.insertToDoToProject(projectPosition, "Study JavaScript", "Review closures and promises", "Next Week", "Medium");
+        instanceOfTodos.insertToDoToProject(projectPosition, "Workout", "Leg day at the gym", "Today", "Low");
+    };
+    const index = document.createElement('h1');
+    index.textContent = `Project ${projectPosition}`;
+    toDoContainer.appendChild(index);
+    todos.forEach((todo) => {
+        const toDoTitle = document.createElement('h2');
+        toDoTitle.textContent = todo.title;
+        toDoContainer.appendChild(toDoTitle);
+    });
 }
