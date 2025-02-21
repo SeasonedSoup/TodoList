e5545f5c9dbfd3f3a3a808ada83e98e021e7a903
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-02-22 05:17:46 +0800

    Minimal Idea Inputed
    so far thinking of an idea on editing todos
    adding an edit button for each and making
    sure the code is clean my code is suddenly
    messy and theres a lot of things being dependent
    so i have to fix that
    
    initial idea as well where i use thje project
    position index and create a toDoIndex in a different
    for loop and posibly improving the new form modal
    where it switches based on the edit button where
    it is clicked and which todo it was so thats about it

--- src/modules/displayTodos.js
+++ src/modules/displayTodos.js
@@ -54,13 +54,24 @@ export const toDoDisplayFunc = (projectPosition) => {
         const removeButton = document.createElement('button');
         removeButton.textContent = 'X'
 
+       
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit'
+
+
         removeButton.addEventListener('click', () => { 
+            //probabaly call form first before doing this where ask if sure or not
             instanceOfTodos.removeToDo(projectPosition, index);
             toDoDisplayFunc(projectPosition);
         });
 
+        editButton.addEventListener('click',  () => {
+            editFormModal();
+        });
+
         toDoItem.appendChild(toDoIndexAndTitle);
         toDoItem.appendChild(removeButton);
+        toDoItem.appendChild(editButton);
 
         toDoContainer.appendChild(toDoItem);
     });
@@ -139,13 +150,68 @@ export const toDoDisplayFunc = (projectPosition) => {
 
             toDoForm.remove();
         })
+    }
         //this will have edit pencil logo for each todo or maybe like a dropdown to see its details
-        const editTodos = () => {
-            const editButton = document.createElement('button');
-            editButton.addEventListener('click',  () => {
+    const editFormModal = () => {
+
+        const toDoFormContainer = document.querySelector('.toDoFormContainer')
+        const existingForm = document.querySelector('.toDoForm');
+
+        if (existingForm) {
+            return;
+        }
+        
+        const toDoForm = document.createElement('form');
+        toDoForm.classList.add('toDoForm');
+    
+        const inputs = [
+            {label: 'ToDo Name:', type:'text', name:'toDoName', id:'toDoName' },
+            {label: 'ToDo Description:', type:'text', name:'toDoDescription', id:'toDoDescription'}, 
+        ];
+    
+        inputs.forEach((inputData) => {
+            const label = document.createElement('label');
+            label.textContent = inputData.label;
+            label.setAttribute('for', inputData.id)
+    
+            const input = document.createElement('input');
+            input.type = inputData.type;
+            input.name = inputData.name;
+            input.id = inputData.id;
+    
+            input.required = true;
+    
+            toDoForm.appendChild(label)
+            toDoForm.appendChild(input)
+            toDoForm.appendChild(document.createElement('br'));     
+        })
+    
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.textContent = 'Create ToDo';
+        toDoForm.appendChild(submitButton);
+            
+        const closeButton = document.createElement('button');
+        closeButton.setAttribute('type', 'button');
+        closeButton.textContent = 'Close';
+        toDoForm.appendChild(closeButton);
+    
+        toDoFormContainer.appendChild(toDoForm);
+            //addeventlisteners
+        closeButton.addEventListener('click', () => {
+            toDoForm.remove();
+        })
+    
+        toDoForm.addEventListener('submit', (e) => {
+            e.preventDefault();
                 
-            });
-        };
+            const toDoName = document.querySelector('#toDoName').value;
+            const toDoDescription = document.querySelector('#toDoDescription').value;
+            instanceOfTodos.insertToDoToProject(projectPosition,toDoName,toDoDescription);
+            toDoDisplayFunc(projectPosition);
+    
+            toDoForm.remove();
+        })
     }
     createToDoHandler();
 }
